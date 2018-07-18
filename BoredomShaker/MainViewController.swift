1cdc7dfeaa9c9a173fcf60ae0b41d942de38597c
Rebekah Baker <rebekah@cratebind.com>
2018-07-18 10:33:49 -0500

    Added saving suggestions list and plist for ideas

--- BoredomShaker/MainViewController.swift
+++ BoredomShaker/MainViewController.swift
@@ -16,33 +16,29 @@ class MainViewController: UIViewController {
     @IBOutlet weak var ideaTwoLabel: UILabel!
     @IBOutlet weak var ideaThreeLabel: UILabel!
     
-    var shakeCount = 0
+    var shakeCount = 1
     let dateFormatter = DateFormatter()
     
+    var suggestions = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.becomeFirstResponder() // To get shake gesture
+        self.becomeFirstResponder()
+        
         self.checkIfUserCanShakeToday()
     }
-    
-    // We are willing to become first responder to get shake motion
+
     override var canBecomeFirstResponder: Bool {
         get {
             return true
         }
     }
-    
-    // Enable detection of shake motion
+
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            shakeCount += 1
             switch shakeCount {
-            case 1:
-                showIdeaLabel(ideaOneLabel, withText: "First Idea")
-            case 2:
-                showIdeaLabel(ideaTwoLabel, withText: "Second Idea")
-            case 3:
-                showIdeaLabel(ideaThreeLabel, withText: "Third Idea")
+            case 1, 2, 3:
+                generateSuggestions()
             case 4:
                 shakeLabel.text = ""
                 outOfIdeasStackView.alpha = 1
@@ -53,31 +49,50 @@ class MainViewController: UIViewController {
                 UserDefaults.standard.synchronize()
             default: return
             }
+            shakeCount += 1
         }
     }
     
+    private func getSavedIdeaForLabel(forKey key: String) -> String {
+        if let idea = UserDefaults.standard.value(forKey: key) {
+            return idea as! String
+        }
+        return ""
+    }
+    
     private func setupShakeUI() {
+        shakeCount = 1
         outOfIdeasStackView.alpha = 0
         ideaOneLabel.alpha = 0
         ideaTwoLabel.alpha = 0
         ideaThreeLabel.alpha = 0
+        checkIfAlteredListNeedsToBeReset()
     }
     
     private func setupNoShakesUI() {
         shakeLabel.text = ""
         outOfIdeasStackView.alpha = 1
-        showIdeaLabel(ideaOneLabel, withText: "First Idea")
-        showIdeaLabel(ideaTwoLabel, withText: "Second Idea")
-        showIdeaLabel(ideaThreeLabel, withText: "Third Idea")
+        showIdeaLabel(ideaOneLabel, withText: getSavedIdeaForLabel(forKey: "IdeaOne"))
+        showIdeaLabel(ideaTwoLabel, withText: getSavedIdeaForLabel(forKey: "IdeaTwo"))
+        showIdeaLabel(ideaThreeLabel, withText: getSavedIdeaForLabel(forKey: "IdeaThree"))
     }
     
     private func showIdeaLabel(_ ideaLabel: UILabel, withText: String) {
         ideaLabel.alpha = 1
         ideaLabel.text = withText
+        switch shakeCount {
+        case 1:
+            UserDefaults.standard.set(ideaLabel.text, forKey: "IdeaOne")
+        case 2:
+            UserDefaults.standard.set(ideaLabel.text, forKey: "IdeaTwo")
+        case 3:
+            UserDefaults.standard.set(ideaLabel.text, forKey: "IdeaThree")
+        default:
+            return
+        }
     }
     
     private func checkIfUserCanShakeToday() {
-        
         if let lastTimeUserShookThreeTimes = UserDefaults.standard.value(forKey: "LastTimeUserShookThreeTimes") {
             
             self.dateFormatter.dateFormat = "MM-dd-yyyy"
@@ -92,8 +107,54 @@ class MainViewController: UIViewController {
             } else {
                 print("User cannot shake today")
                 setupNoShakesUI()
+//              Force SetupShakeUI() For Testing
+//                setupShakeUI()
             }
         }
     }
-
+    
+    private func generateSuggestions() {
+        let randomIdeaIndex = Int(arc4random_uniform(UInt32(suggestions.count)))
+        switch shakeCount {
+        case 1:
+            let ideaOne = suggestions[randomIdeaIndex]
+            showIdeaLabel(ideaOneLabel, withText: ideaOne)
+            
+        case 2:
+            let ideaTwo = suggestions[randomIdeaIndex]
+            showIdeaLabel(ideaTwoLabel, withText: ideaTwo)
+        case 3:
+            let ideaThree = suggestions[randomIdeaIndex]
+            showIdeaLabel(ideaThreeLabel, withText: ideaThree)
+        default:
+            return
+        }
+        removeItemAndSaveAlteredListOfSuggestions(at: randomIdeaIndex)
+        checkIfAlteredListNeedsToBeReset()
+    }
+    
+    private func removeItemAndSaveAlteredListOfSuggestions(at index: Int) {
+        suggestions.remove(at: index)
+        UserDefaults.standard.set(suggestions, forKey: "AlteredIdeas")
+        UserDefaults.standard.synchronize()
+    }
+    
+    private func checkIfAlteredListNeedsToBeReset() {
+        if let alteredSuggestions = UserDefaults.standard.value(forKey: "AlteredIdeas") {
+            if let leftoverList = alteredSuggestions as? [String] {
+                if leftoverList.count > 0 {
+                    suggestions = leftoverList
+                } else {
+                    let pList = Bundle.main.path(forResource: "Suggestions", ofType: "plist")
+                    guard let content = NSDictionary(contentsOfFile: pList!) as? [String:[String]] else {
+                        fatalError()
+                    }
+                    guard let ideas = content["Ideas"] else { fatalError() }
+                    suggestions = ideas
+                }
+            }
+        } else {
+            print("No altered ideas yet")
+        }
+    }
 }
